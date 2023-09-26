e71d4fb1c6dcda3c8291ff26ebbb6bc7e142338f
Charles <k22009879@kcl.ac.uk>
2023-09-26 17:29:43 +0100

    add solution

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,10 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
+from collections import Counter
+
 
 def frequencies(items):
     frequencies = {}
+    frequencies = Counter(items)
     # Your code goes here
     return frequencies
